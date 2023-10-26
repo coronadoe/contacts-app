5a80bac728fee62aec0182fcd24059130111913a
coronadoe <emmanuel.c.coronado@gmail.com>
2023-10-26 13:13:18 -0400

    Inital Commit

---
+++ resources/js/components/MainComponent.vue
@@ -0,0 +1,6 @@
+<template>
+    <h1>Main Component</h1>
+</template>
+
+<script>
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite('resources/js/app.js')
+        <title>Contacts</title>
+    </head>
+    <body>
+        <div id="app">
+            <main-component />
+        </div>
+    </body>
+</html>
