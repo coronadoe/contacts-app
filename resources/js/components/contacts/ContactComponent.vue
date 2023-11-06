0a936361cff753da7151acf2c27ec3e3896c8854
coronadoe <emmanuel.c.coronado@gmail.com>
2023-11-06 00:56:29 -0500

    Added form for data entry

--- resources/js/components/contacts/ContactComponent.vue
+++ resources/js/components/contacts/ContactComponent.vue
@@ -2,6 +2,28 @@
     <div class="contacts-container">
         This will show the contacts container
 
+        <div>
+            <form @submit="onSubmit">
+
+                <div>
+                    <label>First Name</label>
+                    <input type="text" v-model="firstname" name="firstname" placeholder="Add first name" required />
+                </div>
+
+                <div>
+                    <label>Last Name</label>
+                    <input type="text" v-model="lastname" name="lastname" placeholder="Add last name" required />
+                </div>
+
+                <div>
+                    <label>Email</label>
+                    <input type="email" v-model="email" name="email" placeholder="Add email" required />
+                </div>
+
+                <input type="submit" value="Save item" />
+            </form>
+        </div>
+
         <div :key="contact.id" v-for="contact in store.contacts" class="columns-1" >
             <contact-item :contact="contact" />
         </div>
@@ -19,6 +41,23 @@
         components: {
             ContactItem
         },
+
+        data() {
+            return {
+                firstname: '',
+                lastname: '',
+                email: ''
+            }
+        },
+
+        methods: {
+            onSubmit(event) {
+                event.preventDefault();
+
+                console.log(this);
+            }
+        }
+
     });
 </script>
 
--- resources/js/components/contacts/ContactItem.vue
+++ resources/js/components/contacts/ContactItem.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="mx-2 my-2 px-2 py-2 border border-solid rounded-lg border-amber-600 hover:border-amber-300 transition-all duration-300 ease-linear bg-neutral-600 cursor-pointer">
+    <div class="mx-2 my-2 px-2 py-2 border border-solid
+                rounded-lg border-amber-600 hover:border-amber-300 transition-all
+                duration-300 ease-linear bg-neutral-600 cursor-pointer">
         <div> <span>First Name:</span> {{ contact.firstname }}</div>
         <div> <span>Last Name:</span> {{ contact.lastname }}</div>
         <div> <span>Email:</span> {{ contact.email }}</div>
